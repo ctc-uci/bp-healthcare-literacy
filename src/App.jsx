377e7e0cf03be59114942931078aeb1156cbdf28
Charles Lee <charlml1@uci.edu>
2022-02-23 01:06:03 -0800

    added title and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function sayHello() {
+  alert('You clicked me!');
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Healthcare Literacy Resources</h1>
       </header>
+      <button onClick={sayHello} type="button">
+        UCI
+      </button>
+      <button onClick={sayHello} type="button">
+        Options and Coverage
+      </button>
+      <button onClick={sayHello} type="button">
+        Signing Up for Health Insurance
+      </button>
     </div>
   );
 }
