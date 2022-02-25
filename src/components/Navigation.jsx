35ca11e75c46477008a89dc3c77c3604a3f10857
Mignon April Lee <mignonl@uci.edu>
2022-02-25 14:12:25 -0800

    Pages

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            BP Healthcare Literacy
+          </NavLink>
+          <div>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                  <span className="sr-only">(current)</span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/pages/Options">
+                  Options
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/pages/UCSHIP">
+                  UCSHIP
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/pages/SignUp">
+                  Sign Up
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/pages/Home';
+import Options from './components/pages/Options';
+import SignUp from './components/pages/SignUp';
+import UCSHIP from './components/pages/UCSHIP';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Options" element={<Options />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/UCSHIP" element={<UCSHIP />} />
+    </Routes>
+  </Router>,
   document.getElementById('root'),
 );
