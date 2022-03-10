662efc5e749636e2fe53587f39e7b258f2ccc7da
Mandy Feng <mandyfeng21@gmail.com>
2022-03-09 16:06:28 -0800

    options page

---
+++ src/components/pages/Options.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Options() {
+  return (
+    <div className="options">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
+            <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Options</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Options;
