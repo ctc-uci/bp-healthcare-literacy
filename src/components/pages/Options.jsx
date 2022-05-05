5526b7945e423775ffb4774041558a40f1c0ba5c
Mandy Feng <mandyfeng21@gmail.com>
2022-05-04 19:46:27 -0700

    updated options

--- src/components/pages/Options.jsx
+++ src/components/pages/Options.jsx
@@ -11,18 +11,40 @@ function Options() {
             options. In California, you may choose to either have health insurance or go without. If
             you plan on being insured, here are your options.
           </p>
-          <h5 className="font-weight-bold">Parent or Guardian</h5>
+          <h2>Parent or Guardian</h2>
           <p>
             Most people can stay on the healthcare plan of their parent or guardian, usually up
             until the age of 26. This is appealing to a lot of people since it is often cheaper.
             However, it also means that they must be reliant on their parent or guardian.
           </p>
-          <h5 className="font-weight-bold">Employer</h5>
+          <h2>Employer</h2>
           <p>
             Most employers provide affordable healthcare options for their employees. The cost of
             premiums are usually shared between the employer and employee. In addition to healthcare
             insurance, employers often also provide other benefits such as retirement plans and
             dental care.
+          </p>
+          <h2>Spouse</h2>
+          <p>
+            If you are married to a person that has a health insurance plan through their employer,
+            then you are eligible to join the same plan. If you and your spouse both work and are
+            offered coverage from both your employers, then you may choose one plan from the
+            employer that works best for you, or has the best coverage for the best price.
+          </p>
+          <p>
+            Another option if you and your spouse both work is dual coverage. You can both sign up
+            for coverage under your respective employers and cover the both of you with the two
+            plans. Although it may be more expensive, some people prefer this, as it provides more
+            coverage.
+          </p>
+          <h2>Government Programs</h2>
+          <p>
+            There are government programs available to those who cannot access healthcare through an
+            employer or other means. If you live in California, Covered California can connect you
+            to an affordable healthcare plan that works for you. Some plans offer low monthly
+            premiums, but high deductibles, which work best if you rarely visit the doctor but would
+            like to be covered in case you need it. Conversely, other plans offer higher coverage
+            with low or no deductibles, but expect higher premiums.
           </p>
         </div>
       </div>
